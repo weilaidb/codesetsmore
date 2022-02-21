cf00fea920d2200551fd7fc2c27d4e75f4a69f94
yjduoduo <wxjlmr@126.com>
2022-02-21 08:13:58 +0800

    模板类型改成float

--- codesettmore/test/cvectorextpubtest.cpp
+++ codesettmore/test/cvectorextpubtest.cpp
@@ -6,19 +6,19 @@ using namespace testing;
 
 class AnCVectorExtPub: public Test {
 public:
-    CVectorExtPub<int> actor;
-    vector<int> vecempty;
-    vector<int> *pvectest;
+    CVectorExtPub<float> actor;
+    vector<float> vecempty;
+    vector<float> *pvectest;
 
     void SetUp() override
     {
-        int dwLp = 10;
+        float fLp = 10.4;
         pvectest = actor.newVec();
-        pvectest->push_back(dwLp++);
-        pvectest->push_back(dwLp++);
-        pvectest->push_back(dwLp++);
-        pvectest->push_back(dwLp++);
-        pvectest->push_back(dwLp++);
+        pvectest->push_back(fLp++);
+        pvectest->push_back(fLp++);
+        pvectest->push_back(fLp++);
+        pvectest->push_back(fLp++);
+        pvectest->push_back(fLp++);
     }
 
     void TearDown() override
@@ -31,7 +31,7 @@ public:
 
 
 TEST_F(AnCVectorExtPub, printVecReturnSize) {
-    vector<int> vectest;
+    vector<float> vectest;
     ASSERT_THAT(actor.isEmpty(vecempty), Eq(true));
     ASSERT_THAT(actor.printVec(*pvectest), Eq(5));
     ASSERT_THAT(actor.printVecReverse(*pvectest), Eq(5));
