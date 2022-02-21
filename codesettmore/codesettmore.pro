274cbb98870920fca94851b45370275af866f798
yjduoduo <wxjlmr@126.com>
2022-02-21 08:12:10 +0800

    添加模板类测试

--- codesettmore/codesettmore.pro
+++ codesettmore/codesettmore.pro
@@ -3,6 +3,8 @@ CONFIG += console c++11
 CONFIG -= app_bundle
 CONFIG -= qt
 
+DEFINES +=TESTGMOCK
+
 SOURCES += \
         base/define/cdefinepub.cpp \
         base/string/cstringpub.cpp \
--- codesettmore/test/cvectorextpubtest.cpp
+++ codesettmore/test/cvectorextpubtest.cpp
@@ -7,54 +7,45 @@ using namespace testing;
 class AnCVectorExtPub: public Test {
 public:
     CVectorExtPub<int> actor;
-//    vector<int> vecempty;
+    vector<int> vecempty;
     vector<int> *pvectest;
 
     void SetUp() override
     {
-//        int dwLp = 10;
-//        actor.newVec();
-//        pvectest = actor.newVec();
-//        pvectest->push_back(dwLp++);
-//        pvectest->push_back(dwLp++);
-//        pvectest->push_back(dwLp++);
-//        pvectest->push_back(dwLp++);
-//        pvectest->push_back(dwLp++);
+        int dwLp = 10;
+        pvectest = actor.newVec();
+        pvectest->push_back(dwLp++);
+        pvectest->push_back(dwLp++);
+        pvectest->push_back(dwLp++);
+        pvectest->push_back(dwLp++);
+        pvectest->push_back(dwLp++);
     }
 
     void TearDown() override
     {
-//        pvectest->clear();
-//        delete pvectest;
+        pvectest->clear();
+        delete pvectest;
     }
 };
 
-TEST(AnCVectorExtPub, Eg1) {
-//    CVectorExtPub<int> actor;
-//    vector<int> vecempty;
-//    vecempty.clear();
-//    actor.isEmpty(vecempty);
-
-//    ASSERT_THAT(actor.isEmpty(vecempty), Eq(true));
-}
 
 
 TEST_F(AnCVectorExtPub, printVecReturnSize) {
-//    vector<int> vectest;
-//    ASSERT_THAT(actor.isEmpty(vecempty), Eq(true));
-//    ASSERT_THAT(actor.printVec(*pvectest), Eq(5));
-//    ASSERT_THAT(actor.printVecReverse(*pvectest), Eq(5));
-//    ASSERT_THAT(actor.isFull(*pvectest), Eq(true));
+    vector<int> vectest;
+    ASSERT_THAT(actor.isEmpty(vecempty), Eq(true));
+    ASSERT_THAT(actor.printVec(*pvectest), Eq(5));
+    ASSERT_THAT(actor.printVecReverse(*pvectest), Eq(5));
+    ASSERT_THAT(actor.isFull(*pvectest), Eq(true));
 }
 
 
 
 TEST_F(AnCVectorExtPub, SomeInfo) {
-//    ASSERT_THAT(actor.info(vecempty), Eq(0));
-//    ASSERT_THAT(actor.info(*pvectest), Eq(0));
-//    ASSERT_THAT(actor.size(*pvectest), Eq(5));
-//    ASSERT_THAT(actor.capacity(*pvectest), Ge(5));
-//    ASSERT_THAT(actor.max_size(*pvectest), Ge(5));
+    ASSERT_THAT(actor.info(vecempty), Eq(0));
+    ASSERT_THAT(actor.info(*pvectest), Eq(0));
+    ASSERT_THAT(actor.size(*pvectest), Eq(5));
+    ASSERT_THAT(actor.capacity(*pvectest), Ge(5));
+    ASSERT_THAT(actor.max_size(*pvectest), Ge(5));
 }
 
 
